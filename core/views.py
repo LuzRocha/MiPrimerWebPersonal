8f2e4085b5d4d67b7e65f3edcc0cdac497c3d714
Luz Rosales <luzroche199820@gmail.com>
2020-05-15 23:00:49 -0500

    Tercer Avance

--- core/views.py
+++ core/views.py
@@ -5,18 +5,32 @@ html_base = """
 <ul>
   <li><a href="/">Portada</a></li>
   <li><a href="/about-me/">Acerca de</a></li>
+  <li><a href="/portfolio/">portafolio</a></li>
+  <li><a href="/contact/">Contacto</a></li>
 </ul>  
 """
 
 # Create your views here.
 def home(request):
     return HttpResponse(html_base + """
-    "<h2>Portada</h2>
+    <h2>Portada</h2>
     <p>Esto es la portada</p)
     """)
 
 def about(request):
     return HttpResponse(html_base + """
-       <h2>Acerca de</h2>
-    <P>Me llamo luz y soy programadora</p>)
+       <h2>Acerca de </h2>
+    <P>Me llamo luz y soy programadora</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+       <h2>portafolio</h2>
+    <p>algunos de mis trabajos</p)
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+       <h2>Contacto</h2>
+    <P>Aqui les dejo mi e-mail para preguntarme dudas: <a href="malto:hola@luz.net">hola@luz.net</p>
     """)
