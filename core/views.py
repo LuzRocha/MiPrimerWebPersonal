71ce8847392198d1b679e21dfd579472a95d0b89
Luz Rosales <luzroche199820@gmail.com>
2020-05-15 22:07:31 -0500

    Segundo Avance

--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+  <li><a href="/">Portada</a></li>
+  <li><a href="/about-me/">Acerca de</a></li>
+</ul>  
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Mi web personal</h1><h2>Portada</h2")
+    return HttpResponse(html_base + """
+    "<h2>Portada</h2>
+    <p>Esto es la portada</p)
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+       <h2>Acerca de</h2>
+    <P>Me llamo luz y soy programadora</p>)
+    """)
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -19,5 +19,6 @@ from core import views
 
 urlpatterns = [
     path("",views.home, name="home"),
+    path("about-me/", views.about, name="about"),
     path('admin/', admin.site.urls),
 ]
