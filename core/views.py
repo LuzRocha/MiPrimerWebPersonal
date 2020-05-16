510fa53c0de5a0460ece9a8273ef49ff8bb1a33d
Luz Rosales <luzroche199820@gmail.com>
2020-05-16 00:08:09 -0500

    Cuarto Avance

--- core/views.py
+++ core/views.py
@@ -1,36 +1,17 @@
 from django.shortcuts import render, HttpResponse
+ 
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-  <li><a href="/">Portada</a></li>
-  <li><a href="/about-me/">Acerca de</a></li>
-  <li><a href="/portfolio/">portafolio</a></li>
-  <li><a href="/contact/">Contacto</a></li>
-</ul>  
-"""
 
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-    <h2>Portada</h2>
-    <p>Esto es la portada</p)
-    """)
+    return render(request,"core/home.html")
+    
 
 def about(request):
-    return HttpResponse(html_base + """
-       <h2>Acerca de </h2>
-    <P>Me llamo luz y soy programadora</p>
-    """)
+   return render(request,"core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-       <h2>portafolio</h2>
-    <p>algunos de mis trabajos</p)
-    """)
+   return render(request,"core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-       <h2>Contacto</h2>
-    <P>Aqui les dejo mi e-mail para preguntarme dudas: <a href="malto:hola@luz.net">hola@luz.net</p>
-    """)
+    return render(request,"core/contact.html")
